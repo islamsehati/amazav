893a9ddddc8098fd9efd42a60a3a0527b49701f1
mangunwirayuda <dapatin.id@gmail.com>
2025-06-19 23:12:44 +0700

    Update WA and select CS

--- app/Livewire/ArmadaDetail.php
+++ app/Livewire/ArmadaDetail.php
@@ -3,16 +3,25 @@
 namespace App\Livewire;
 
 use App\Models\Armada;
+use Carbon\Carbon;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class ArmadaDetail extends Component
 {
     public $slug;
+    #[Url()]
     public $nama;
+    #[Url()]
     public $noWA;
+    #[Url()]
     public $tglAwal;
+    #[Url()]
     public $tglAkhir;
+    #[Url()]
     public $tujuan;
+    #[Url()]
+    public $cust_service;
 
     public function mount($slug)
     {
@@ -21,8 +30,46 @@ class ArmadaDetail extends Component
 
     public function render()
     {
+        $hariAwal = Carbon::parse($this->tglAwal)->format('l');
+        if ($hariAwal === 'Sunday') {
+            $hariAwl = 'Ahad';
+        } elseif ($hariAwal === 'Monday') {
+            $hariAwl = 'Senin';
+        } elseif ($hariAwal === 'Tuesday') {
+            $hariAwl = 'Selasa';
+        } elseif ($hariAwal === 'Wednesday') {
+            $hariAwl = 'Rabu';
+        } elseif ($hariAwal === 'Thursday') {
+            $hariAwl = 'Kamis';
+        } elseif ($hariAwal === 'Friday') {
+            $hariAwl = 'Jumat';
+        } elseif ($hariAwal === 'Saturday') {
+            $hariAwl = 'Sabtu';
+        }
+        // dd($hariAwl);
+
+        $hariAkhir = Carbon::parse($this->tglAkhir)->format('l');
+        if ($hariAkhir === 'Sunday') {
+            $hariAkh = 'Ahad';
+        } elseif ($hariAkhir === 'Monday') {
+            $hariAkh = 'Senin';
+        } elseif ($hariAkhir === 'Tuesday') {
+            $hariAkh = 'Selasa';
+        } elseif ($hariAkhir === 'Wednesday') {
+            $hariAkh = 'Rabu';
+        } elseif ($hariAkhir === 'Thursday') {
+            $hariAkh = 'Kamis';
+        } elseif ($hariAkhir === 'Friday') {
+            $hariAkh = 'Jumat';
+        } elseif ($hariAkhir === 'Saturday') {
+            $hariAkh = 'Sabtu';
+        }
+        // dd($hariAkh);
+
         return view('livewire.armada-detail', [
             'armada' => Armada::where('slug', $this->slug)->firstOrFail(),
+            'hariAwl' => $hariAwl,
+            'hariAkh' => $hariAkh,
         ]);
     }
 }
--- resources/views/livewire/armada-detail.blade.php
+++ resources/views/livewire/armada-detail.blade.php
@@ -1,4 +1,4 @@
-<div class="md:space-y-6 space-y-3 max-w-2xl mx-auto">
+<div class="md:space-y-6 space-y-3 max-w-2xl mx-auto pb-10">
 
     @php
     foreach ( $armada->images as $image) {
@@ -44,20 +44,48 @@
             <x-input type="datetime-local" wire:model.live="tglAkhir" label="Tanggal Akhir" />
         </div>
         <x-input wire:model.live="tujuan" label="Tujuan"/>
-        <div class="font-mono mt-4 text-sm">lengkapi data diatas dan tekan Ajukan</div>
+
+        <div class="mb-1">Pilih Kontak</div>
+        <ul class="grid xs:grid-cols-2 grid-cols-1 w-full gap-2">
+            <li class="group">
+                <input  wire:model.live='cust_service' class="hidden peer" id="CS01" required="" type="radio" value="6285183059925" />
+                <label class="inline-flex items-center justify-between w-full p-3 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:group-hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 group-hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:group-hover:bg-gray-700" for="CS01">
+                    <div class="block">
+                        <div class="relative z-10 w-full pr-1 text-xl font-semibold bg-white rounded-sm dark:bg-gray-800 dark:group-hover:bg-gray-700 xs:text-lg group-hover:bg-gray-100">
+                            Fani - Admin
+                        </div>
+                    </div>
+                    <span style="scale: 2; margin-right:0.5rem;"><x-icon name="user" class="size-4" /></span>
+                </label>
+                </input>
+            </li>
+            <li class="group">
+                <input wire:model.live='cust_service' class="hidden peer" id="CS02" type="radio" value="6283820634317">
+                <label class="inline-flex items-center justify-between w-full p-3 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:group-hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 group-hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:group-hover:bg-gray-700" for="CS02">
+                    <div class="block">
+                        <div class="relative z-10 w-full pr-1 text-xl font-semibold bg-white rounded-sm dark:bg-gray-800 dark:group-hover:bg-gray-700 xs:text-lg group-hover:bg-gray-100">
+                            Yuri - Driver
+                        </div>
+                    </div>
+                    <span style="scale: 2; margin-right:0.5rem;"><x-icon name="users" class="size-4" /></span>
+                </label>
+                </input>
+            </li>
+        </ul>
+        <div class="font-mono mt-4 text-sm">*lengkapi data di atas dan tekan Ajukan</div>
     </div>
 
     @php
-    $textPesan = "Halo AMAZAV%0D%0ASaya *".Str::title($this->nama)."* ingin melakukan booking _".$armada->name."_ pada ".date('l d-M-Y H:i a', strtotime($this->tglAwal))." s.d ".date('l d-M-Y H:i a', strtotime($this->tglAkhir))." tujuan *".Str::title($this->tujuan)."* %0D%0A%0D%0AHub saya kembali : ".$this->noWA;
+    $textPesan = "Halo AMAZAV%0D%0ASaya *".Str::title($this->nama)."* ingin melakukan booking armada :  %0D%0A_".$armada->name."_ %0D%0A%0D%0Apada : %0D%0A".$hariAwl." ".date('d-M-Y H:i', strtotime($this->tglAwal))." s.d %0D%0A".$hariAkh." ".date('d-M-Y H:i', strtotime($this->tglAkhir))."%0D%0A%0D%0ATujuan : %0D%0A*".Str::title($this->tujuan)."* %0D%0A%0D%0AHub saya kembali : ".$this->noWA;
     @endphp
 
-    @if ($nama == null || $noWA == null || $tglAwal == null || $tglAkhir == null || $tujuan == null)        
+    @if ($nama == null || $noWA == null || $tglAwal == null || $tglAkhir == null || $tujuan == null || $cust_service == null)        
     <div class="w-full ">
         <x-button text="Ajukan Sekarang" color="gray" light class="w-full" />
     </div>
     @else
-    <div class="w-full ">
-        <a href="https://wa.me/6285950540055?text={{ $textPesan }}"  target="_blank"><x-button text="Ajukan Sekarang" color="green" light class="w-full" /></a>
+    <div class="w-full">
+        <a href="https://wa.me/{{ $this->cust_service }}?text={{ $textPesan }}"  target="_blank"><x-button text="Ajukan Sekarang" color="green" light class="w-full" /></a>
     </div>
     @endif
 </div>
