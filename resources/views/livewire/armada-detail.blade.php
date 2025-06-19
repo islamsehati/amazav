6996e41ca2cd8284bdda7dfdf645aaf414936652
mangunwirayuda <dapatin.id@gmail.com>
2025-06-19 16:11:30 +0700

    Update Carousel Armada Detail

--- resources/views/livewire/armada-detail.blade.php
+++ resources/views/livewire/armada-detail.blade.php
@@ -1,3 +1,17 @@
-<div>
+<div class="md:space-y-6 space-y-3 max-w-2xl mx-auto">
+
+    @php
+    foreach ( $armada->images as $image) {
+        $imgArmd[] = [
+            'src' => Str::replace('%2F', '/',url('storage', $image)),
+            'alt' => 'armada',
+        ];
+    }
+    // dd($imgArmd);
+    @endphp
+
+            <x-carousel autoplay stop-on-hover wrapper="aspect-square"
+            :images="$imgArmd"
+            />
     <span class="dark:text-white">{{ $armada->name }} ..</span>
 </div>
